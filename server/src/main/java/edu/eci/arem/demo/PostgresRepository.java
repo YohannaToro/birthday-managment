20383ee03f372fea22c9412d25405f031d8be404
YohannaToro <yoha_duran156@hotmail.com>
2019-12-08 19:59:10 -0500

    arreglar configuracion

--- server/src/main/java/edu/eci/arem/demo/PostgresRepository.java
+++ server/src/main/java/edu/eci/arem/demo/PostgresRepository.java
@@ -73,32 +73,45 @@ public class PostgresRepository implements SaludoPersistence {
 		}
     }
 
+
 	@Override
 	public void Update(Saludo sa) throws SQLException {
-		// TODO Auto-generated method stub
+		System.out.println(sa);
+		String na=sa.getDescription(); String ap=sa.getApellido();
+		String di=Integer.toString(sa.getDia());String me=Integer.toString(sa.getMes());
+		String pe=sa.getPerfil();String co=sa.getCorreo(); String ob=sa.getObservacion();
+		//String q="update into informacion set set dia='"+di+"';
+		String query = "UPDATE informacion set dia='"+di+"', mes='"+me+"', observacion='"+ob+"', correo='"+co+"', perfil='"+pe+"' where apellido='"+ap+"';";
+		System.out.println("query"+query);
+		Connection connection = null;
+		try {
+			connection = dataSource.getConnection();
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+        }catch (Exception e) {
+			System.out.println(e.getMessage());
+			throw new RuntimeException(e);
+		}
+		
 
 	}
 
 	@Override
 	public Saludo getUser(String name) {
-		String query = "SELECT * FROM informacion where apellido="+name+";";
+		System.out.println("nombre"+name);
+		String query = "SELECT * FROM informacion where apellido='"+name+"';";
+		System.out.println(query);
 		Connection connection = null;
 		try {
-			connection = dataSource.getConnection();
-			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
+			Saludo auction = new Saludo();
+			List<Saludo> auctions = findAll();
+			for (int i=0; i<auctions.size();i++){
+				Saludo tmp=auctions.get(i);
+				if (tmp.getApellido().equals(name)){
+					auction=tmp;
+				};
+			};
 			
-				Saludo auction = new Saludo();
-				//auction.setDescription(rs.getString("nombre"));
-				auction.setApellido(rs.getString("apellido"));
-				auction.setDia(Integer.parseInt(rs.getString("dia")));
-				auction.setMes(Integer.parseInt(rs.getString("mes")));
-				auction.setCorreo(rs.getString("correo"));
-				auction.setPerfil(rs.getString("perfil"));
-				auction.setObservacion(rs.getString("observacion"));
-
-
-			connection.close();
 			return auction;
 		} catch (Exception e) {
 			// TODO: handle exception
--- server/src/main/java/edu/eci/arem/demo/controller/Controller.java
+++ server/src/main/java/edu/eci/arem/demo/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -47,6 +48,17 @@ public class Controller {
 		
 		 try {
 	        auctionRepository.create(sa);
+		
+		 return new ResponseEntity<>("done", HttpStatus.ACCEPTED);
+	    } catch (Exception ex) {
+	        return new ResponseEntity<>("no hizo post",HttpStatus.NOT_FOUND);
+	    }
+	}
+	@PutMapping(value="/users/update")
+	public ResponseEntity<?> updateMethodName(@RequestBody Saludo sa) {
+		
+		 try {
+	        auctionRepository.Update(sa);
 		
 		 return new ResponseEntity<>("done", HttpStatus.ACCEPTED);
 	    } catch (Exception ex) {
